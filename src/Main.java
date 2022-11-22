439ee8c9967ad1090e771a272693a2f1998d41aa
lis76 <rsavoskin@yandex.ru>
2022-11-22 20:16:24 +0300

    First example of tape casting

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Object o = new Scanner(System.in);
+
+
+
+        Scanner scanner = null;
+
+        if (o instanceof Scanner) scanner = (Scanner) o;
+
+        if (scanner != null) {
+            scanner.nextInt();
+        }
+
+
+        Object o1 = new String();
+
+        Object o2 = new Integer(15);
+
     }
 }
