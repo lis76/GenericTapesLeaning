87ed078a5400ce74fd466f64fe938ab7d08c59ac
lis76 <rsavoskin@yandex.ru>
2022-11-24 10:48:25 +0300

    Finished with instanceof

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Object o = new Scanner(System.in);
+
+//        Object o = new Scanner(System.in);
+//
+//
+//
+//        Scanner scanner = null;
+//
+//        if (o instanceof Scanner) scanner = (Scanner) o;
+//
+//        if (scanner != null) {
+//            scanner.nextInt();
+//        }
+//
+//
+//        Object o1 = new String();
+//
+//        Object o2 = new Integer(15);
 
 
-
-        Scanner scanner = null;
-
-        if (o instanceof Scanner) scanner = (Scanner) o;
-
-        if (scanner != null) {
-            scanner.nextInt();
+        Object[] objects = {10, "Привет", 3.14}; //Здесь происходит автоупаковка.
+        for (Object o : objects) {
+            if (o instanceof String){
+                String s = (String) o;
+                System.out.println(s);
+            }
         }
 
 
-        Object o1 = new String();
 
-        Object o2 = new Integer(15);
 
     }
 }
