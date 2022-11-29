7e112cacef911c861b6817464eeb8759bc422ae8
lis76 <rsavoskin@yandex.ru>
2022-11-29 11:27:39 +0300

    Refactored T to interface lvl and added class support

--- src/customgenerictypes/Account.java
+++ src/customgenerictypes/Account.java
@@ -1,19 +1,19 @@
 package customgenerictypes;
 
-public class Account implements Accountable<String> {
-    private String id;
+public class Account<T> implements Accountable<T> {
+    private T id;
     private int sum;
 
-    public Account(String id, int sum) {
+    public Account(T id, int sum) {
         this.id = id;
         this.sum = sum;
     }
 
-    public String getId() {
+    public T getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(T id) {
         this.id = id;
     }
 
