0299ec6ffff6dd3937e7fba7a710f1238abd07b4
lis76 <rsavoskin@yandex.ru>
2022-11-29 11:07:28 +0300

    Refactored to Generic Class

--- src/customgenerictypes/Main.java
+++ src/customgenerictypes/Main.java
@@ -2,13 +2,13 @@ package customgenerictypes;
 
 public class Main {
     public static void main(String[] args) {
-        Account acc1 = new Account(12345, 5000);
+        Account <Integer> acc1 = new Account(12345, 5000);
         System.out.println(acc1.getId());
 
-        Account acc2 = new Account("sid1234563", 5000);
+        Account<String> acc2 = new Account("sid1234563", 5000);
         System.out.println(acc2.getId());
 
-        int a = ((Integer) acc2.getId()) + 10;
+        int a = acc1.getId() + 10; //Опасное преобразование типов
         System.out.println(a);
     }
 }
