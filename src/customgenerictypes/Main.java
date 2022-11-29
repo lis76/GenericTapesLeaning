91a207fb5044e47bdd6cf9e75de24948725b4d7c
lis76 <rsavoskin@yandex.ru>
2022-11-29 10:58:37 +0300

    Example of not working Object cast

--- src/customgenerictypes/Main.java
+++ src/customgenerictypes/Main.java
@@ -2,6 +2,13 @@ package customgenerictypes;
 
 public class Main {
     public static void main(String[] args) {
+        Account acc1 = new Account(12345, 5000);
+        System.out.println(acc1.getId());
 
+        Account acc2 = new Account("sid1234563", 5000);
+        System.out.println(acc2.getId());
+
+        int a = ((Integer) acc2.getId()) + 10;
+        System.out.println(a);
     }
 }
