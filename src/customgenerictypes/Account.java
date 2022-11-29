ef1567748ef8ddcf3964228efabc8cd8a7e4a078
lis76 <rsavoskin@yandex.ru>
2022-11-29 12:30:57 +0300

    Constructor only generic implementation

--- src/customgenerictypes/Account.java
+++ src/customgenerictypes/Account.java
@@ -3,6 +3,12 @@ package customgenerictypes;
 public class Account<T> implements Accountable<T> {
     private T id;
     private int sum;
+    private Object smth;
+
+    <S> Account(T id, int sum, S smth) {
+        this(id, sum);
+        this.smth = smth;
+    }
 
     public Account(T id, int sum) {
         this.id = id;
--- src/customgenerictypes/Main.java
+++ src/customgenerictypes/Main.java
@@ -10,5 +10,7 @@ public class Main {
 
         int a = acc1.getId() + 10; //Опасное преобразование типов
         System.out.println(a);
+
+        Account<Integer> acc3 = new Account(13000, 100, "Something");
     }
 }
